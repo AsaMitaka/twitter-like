bed44afcd23e9197fd77d14e65c2698ac74bdd63
AsaMitaka <cepelits@gmail.com>
2024-01-03 20:56:39 +0200

    * app routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import { Main, Sign } from './pages/';
+import {
+  MainPage,
+  SignPage,
+  TweetPage,
+  ProfilePage,
+  NotificationPage,
+  ExplorePage,
+} from './pages/';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/sign" element={<Sign />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/sign" element={<SignPage />} />
+        <Route path="/tweet/:id" element={<TweetPage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
     </>
   );
