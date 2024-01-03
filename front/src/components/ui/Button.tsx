942683c85f14e202e711aae0ba2c166b6ea84aab
AsaMitaka <cepelits@gmail.com>
2024-01-03 20:57:42 +0200

    + button ui comp

--- front/src/components/ui/Button.tsx
+++ front/src/components/ui/Button.tsx
@@ -14,15 +14,14 @@ const Button: FC<ButtonProps> = ({ actionLabel, big, minWidth, onClick, outline,
     <button
       className={`
       px-3 py-2 transition duration-200 disabled:opacity-70 disabled:cursor-not-allowed
-      ${big ? 'w-full justify-center font-bold text-xl' : 'min-w-[6rem] max-w-full'} 
+      ${big ? 'w-full justify-center font-bold text-xl' : ''} 
       ${minWidth ? 'bg-sky-500 justify-center hover:bg-sky-600' : ''} 
       ${
         outline
-          ? 'text-sky-500 bg-white border-2 text-black border-black hover:bg-gray-100'
-          : 'border-2 border-sky-500 bg-sky-500 text-white'
+          ? 'text-black font-bold bg-white border-2 text-black border-black hover:bg-gray-100'
+          : 'border-2 font-bold border-sky-500 bg-sky-500 text-white hover:opacity-70'
       }
       ${rounded ? 'rounded-full' : ''}
-
       `}
       onClick={onClick}>
       {actionLabel}
