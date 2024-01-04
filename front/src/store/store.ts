22ac0877a1ce5d6ea6b93066da9ac076a857facd
AsaMitaka <cepelits@gmail.com>
2024-01-04 02:30:36 +0200

    + store redux toolkit

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,5 +1,17 @@
-import { createStore } from "redux";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userSliceReducer from './slices/userSlice';
+import tweetsSliceReducer from './slices/tweetsSlice';
+import { useDispatch } from 'react-redux';
 
-export const createStore({
-
+const rootReducer = combineReducers({
+  user: userSliceReducer,
+  tweets: tweetsSliceReducer,
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
