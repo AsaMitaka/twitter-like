12678a60ec296320937672823db32def18281978
AsaMitaka <cepelits@gmail.com>
2024-01-04 02:30:10 +0200

    * private route

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,17 +7,20 @@ import {
   NotificationPage,
   ExplorePage,
 } from './pages/';
+import PrivateRoute from './pages/PrivateRoute';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/tweet/:id" element={<TweetPage />} />
+          <Route path="/profile/:id" element={<ProfilePage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+        </Route>
         <Route path="/sign" element={<SignPage />} />
-        <Route path="/tweet/:id" element={<TweetPage />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
     </>
   );
